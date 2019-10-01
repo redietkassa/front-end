f805e102b0dcd831ab92ce009460ad57ba768c56
redietkassa <56015936+redietkassa@users.noreply.github.com>
2019-10-01 09:55:41 +0300

    initial comit

---
+++ Listcontroller.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+
+class Listcontroller extends Controller
+
+{
+  public function show()
+{
+$characters=[
+'addisu'=>'forgery',
+'tesfaye'=>'assault',
+'aklilu'=>'theft',
+'abera'=> 'murder'
+];
+return view('welcome')->withCharacters($characters);
+}
+
+}
